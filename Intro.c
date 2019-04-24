1ee2509b92cadf7170d025ea3ef67d8dc3193427
tryingtokeepup <kl.hawaii@gmail.com>
2019-04-24 14:10:41 -0700

    first commit

---
+++ Intro.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+    // pid_t pid = fork();
+
+    // if (pid == 0) {
+
+    //     execlp("cd ..", "cd ..", NULL)
+    //     perror("exec")
+    // }
+
+    return 0;
+}
